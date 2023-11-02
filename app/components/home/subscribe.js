ea05a63e49a66fc60467967ef4e3611a8719586c
PensivePasta <pensivepasta@gmail.com>
2023-11-02 16:14:14 +0000

    add subscribe component

---
+++ app/components/home/subscribe.js
@@ -0,0 +1,14 @@
+const Subscribe = () => {
+  return (
+    <div className="subscribe-container">
+      <h2>SUBSCRIBE</h2>
+      <p>Sign-up to survive your next dungeon crawl.</p>
+      <div className="input-group">
+        <input type="email" placeholder="Enter your email address" />
+        <button type="submit">SUBMIT</button>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import HomeHero from "./components/home/home-hero";
+import Subscribe from "./components/home/subscribe";
 
 const Home = () => {
   return (
       <div>
         <HomeHero />
+        <Subscribe />
       </div>
   );
 }
