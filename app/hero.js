3ad3f7689f66e36e138e604942bc8f9c11c1b58e
PensivePasta <pensivepasta@gmail.com>
2023-11-02 19:23:01 +0000

    style hero

--- app/hero.js
+++ app/hero.js
@@ -1,22 +1,19 @@
+import "./homepage.css";
+
 const Hero = ({ title, subtitle, imageUrl }) => {
-    return (
-      <section className="hero" style={{ 
-          backgroundImage: `url(${imageUrl})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '600px',
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-        <div className="content">
-          <h1>{title}</h1>
-          {subtitle && <h3>{subtitle}</h3>}
-        </div>
-      </section>
-    );
-  };
-  
-  export default Hero;
-  
+  return (
+    <section
+      className="hero"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    >
+      <div className="content">
+        <h1>{title}</h1>
+        {subtitle && <h3>{subtitle}</h3>}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
