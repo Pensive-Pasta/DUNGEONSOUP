a14f9659ce2ca6ed6f79467504789dc627061d24
PensivePasta <pensivepasta@gmail.com>
2023-11-05 14:33:48 +0000

    added fetch for latest articles, load more functionality and hrefs

--- app/components/article-card.js
+++ app/components/article-card.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ArticleCard = ({ title, subtitle = "", imageUrl, href }) => {
+const ArticleCard = ({
+  title,
+  subtitle = "",
+  imageUrl,
+  article_id,
+  author_id,
+}) => {
   return (
     <div className="article-card">
-      <Link href={href}>
+      <Link href={`/${author_id}/${article_id}`}>
         <div className="article-image">
           <Image src={imageUrl} alt={title} width={300} height={200} />
         </div>
--- app/components/home/latest-articles.js
+++ app/components/home/latest-articles.js
@@ -1,23 +1,40 @@
+"use client";
+import { useEffect, useState } from "react";
 import ArticleCard from "../article-card";
 
 const LatestArticles = () => {
-  const articles = [
-    {
-      title: "The Art of Negotiation: Convincing That Troll to Let You Pass",
-      snippet: "Learn the secrets of negotiating with mythical creatures...",
-      imageUrl: "/path/to/image1.jpg",
-      href: "/",
-    },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [articlePagination, setArticlePagination] = useState(3);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/articles")
+      .then((response) => response.text())
+      .then((data) => {
+        setLoading(false);
+        setArticles(JSON.parse(data));
+        return;
+      })
+      .catch((error) => console.error("Error fetching author data:", error));
+  }, []);
 
   return (
     <div id="articles">
       <h2>LATEST ARTICLES</h2>
-      <div className="articles-list">
-        {articles.map((article, index) => (
-          <ArticleCard key={index} {...article} />
-        ))}
-      </div>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <div className="articles-list">
+          {articles.slice(0, articlePagination).map((article, index) => (
+            <ArticleCard key={index} {...article} />
+          ))}
+          {articles.length > articlePagination && (
+            <button onClick={() => setArticlePagination(articlePagination + 6)}>
+              Load more
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
