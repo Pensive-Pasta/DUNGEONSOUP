839d6574d76d1035e986efaaba8aa07d5c97c250
PensivePasta <pensivepasta@gmail.com>
2023-11-08 18:43:19 +0000

    Add font import to head link

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,12 @@ import "./global.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;700&family=Roboto:wght@100;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Navbar />
         {children}
