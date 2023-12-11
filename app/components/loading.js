12f3aed78eee28389bc0d6943b950fbc469fb86a
PensivePasta <pensivepasta@gmail.com>
2023-12-11 19:44:20 +0000

    Add spinner animation to loading message.

--- app/components/loading.js
+++ app/components/loading.js
@@ -1,5 +1,16 @@
 const Loading = () => {
-  return <div className="loading-container">Loading...</div>;
-};
+  return (
+      <div className="loading">
+
+      <div className="spinner"></div>
+      <p>
+      Waking up the backend.
+      <br />
+      Please allow a minute. 😴
+      </p>
+
+    </div>
+  );
+}
 
 export default Loading;
